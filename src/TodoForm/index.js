831196cf1b7366b102013ead8492fe65ad24dd21
pilar <ingeniera.coral@gmail.com>
2022-05-01 14:04:57 -0500

    Cambios todoFormat -- Se edita formato para que no deje guardar cambios si el campo se encuentra vacío

--- src/TodoForm/index.js
+++ src/TodoForm/index.js
@@ -4,17 +4,28 @@ import "./TodoForm.css";
 
 function TodoForm() {
   const [newTodoValue, setNewTodoValue] = React.useState("");
+  const [isEmptyText, setIsEmptyText] = React.useState(true);
+  const [alert, setAlert] = React.useState(false);
+
   const { addTodo, setOpenModal, enEdit, setEnEdit, editTodo, textEdit } =
     React.useContext(TodoContext);
 
   const onChange = (event) => {
     setNewTodoValue(event.target.value);
+    if (newTodoValue != "") {
+      setIsEmptyText(false);
+    }
   };
   const onCancel = () => {
     setOpenModal(false);
+    setEnEdit(false);
   };
   const onSubmit = (event) => {
     event.preventDefault();
+    if (isEmptyText) {
+      setAlert(true);
+      return;
+    }
     if (!enEdit) {
       addTodo(newTodoValue);
     } else {
@@ -22,16 +33,32 @@ function TodoForm() {
       setEnEdit(false);
     }
     setOpenModal(false);
+    setNewTodoValue("");
+    setIsEmptyText(true);
   };
 
   return (
     <form onSubmit={onSubmit}>
-      <label>Escribe tu nuevo TODO</label>
-      <textarea
-        value={newTodoValue}
-        onChange={onChange}
-        placeholder="Tarea pendiente"
-      />
+      {(enEdit && <label>Edita tu TODO</label>) || (
+        <label>Escribe tu nuevo TODO</label>
+      )}
+
+      {(enEdit && (
+        <textarea
+          value={newTodoValue}
+          onChange={onChange}
+          placeholder={textEdit}
+        />
+      )) || (
+        <textarea
+          value={newTodoValue}
+          onChange={onChange}
+          placeholder="Tarea pendiente"
+        />
+      )}
+
+      {alert && <h3>Debes agregar una tarea</h3>}
+
       <div className="TodoForm-buttonContainer">
         <button
           type="button"
@@ -41,7 +68,7 @@ function TodoForm() {
           Cancelar
         </button>
         <button type="submit" className="TodoForm-button TodoForm-button--add">
-          Añadir
+          Guardar
         </button>
       </div>
     </form>
